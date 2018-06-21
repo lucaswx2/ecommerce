57b6bb14d75870606be6a0e5656378fed73c04a8
Lucas Medeiros <freitasmlucas@hotmail.com>
2018-06-21 07:28:02 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { AbasPage } from '../pages/abas/abas';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AbasPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AbasPage,
+
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule { }
---
+++ src/pages/abas/abas.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+/**
+ * Generated class for the AbasPage tabs.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-abas',
+  templateUrl: 'abas.html'
+})
+export class AbasPage {
+
+  inicioRoot = 'InicioPage'
+  categoriasRoot = 'CategoriasPage'
+  favoritosRoot = 'FavoritosPage'
+  minhaContaRoot = 'MinhaContaPage'
+
+
+  constructor(public navCtrl: NavController) {}
+
+}
